c7d48096a3f8ee839b6f909845acd1cc3f8463fb
Eduardo <eduardopires31@gmail.com>
2023-10-18 15:33:32 -0300

    Added housing components with details routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [HomeComponent, RouterModule]
 })
 export class AppComponent {
-  title = 'homes-app';
+  title = 'Hello World!';
 }
